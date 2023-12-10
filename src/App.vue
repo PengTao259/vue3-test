45398c98574af34c326f2d0d9c3c23217ff4e2d3
pengtaonumber@gmail.com <pengtaonumber@gmail.com>
2023-12-10 11:00:07 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+import sonComVue from '@/components/son-com.vue'
+const money = ref(100)
+const changeMoney = () => {
+  money.value += 100
+}
+const payMoney = (num) => {
+  money.value -= num
+}
+</script>
+<template>
+  <h3>
+    父组件 - {{ money }}
+    <button @click="changeMoney">挣钱</button>
+    <sonComVue :money="money" @changeMoney="payMoney"></sonComVue>
+  </h3>
+</template>
---
+++ src/components/son-com.vue
@@ -0,0 +1,23 @@
+<script setup>
+const props = defineProps({
+  money: Number
+})
+const emit = defineEmits(['changeMoney'])
+const payMoney = () => {
+  emit('changeMoney', 10)
+}
+</script>
+
+<template>
+  <div class="son">
+    <h3>子组件 - {{ money }}</h3>
+    <button @click="payMoney">花钱</button>
+  </div>
+</template>
+
+<style scoped>
+.son {
+  border: 1px solid #000;
+  padding: 20px;
+}
+</style>
